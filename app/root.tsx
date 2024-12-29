bd3b90f6b5945ce216abfeef4029c6bf4335658a
front_조수진 <jo.sujin@cashwalk.io>
2024-12-29 23:28:21 +0900

    chore: tailwind 설정

--- app/root.tsx
+++ app/root.tsx
@@ -1,27 +1,28 @@
+import { Outlet, Scripts, Links } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "./routes/styles/tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
+        <Links />
         <style>{`
           button {
             border: 0;
           }
         `}</style>
       </head>
-      <body>안녕하세요.</body>
+      <body className="w-[100vw] h-[100vh] content-center items-center p-5">
+        <Outlet />
+        <Scripts />
+      </body>
     </html>
   );
 }
